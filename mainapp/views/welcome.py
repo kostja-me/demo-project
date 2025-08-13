85b6faff5b08fa41902c8e1a61bb5cab4db46f69
Kostja P <git@kostja.me>
2025-08-13 13:55:04 +0200

    print remote address

--- mainapp/views/welcome.py
+++ mainapp/views/welcome.py
@@ -5,5 +5,10 @@ class WelcomeToSpeedPyView(TemplateView):
     template_name = "mainapp/welcome.html"
 
 
+    def get(self, request, *args, **kwargs):
+        print(request.META)
+        return super().get(request, *args, **kwargs)
+
+
 class PricingView(TemplateView):
     template_name = "mainapp/pricing.html"
